05013c888745271a97ecd62e0c0663d6bf94e6d5
Maria <mlovatonv@gmail.com>
2020-09-11 15:54:15 -0500

    add extra tests

--- src/cache.cpp
+++ src/cache.cpp
@@ -6,6 +6,7 @@ template <typename t_key, typename t_value>
 void cache<t_key, t_value>::insert_key_value_pair(t_key key, t_value value) {
   if (this->size == this->max_size) {
     this->evict();
+    --this->size;
   }
   if (this->get_value_from_key(key) == nullptr) {
     ++this->size;
--- test/lrucachetest.cpp
+++ test/lrucachetest.cpp
@@ -36,7 +36,7 @@ TEST(lrucachetest, list_should_work) {
 
   l.pop_front();
   l.pop_front();
-  // l.pop_front(); // extra pop_front
+  l.pop_front(); // extra pop_front
 
   EXPECT_EQ(l.front(), nullptr);
   EXPECT_EQ(l.back(), nullptr);
@@ -44,7 +44,7 @@ TEST(lrucachetest, list_should_work) {
 
 TEST(lrucachetest, lrucache_should_work) {
   using namespace ads;
-  lrucache<char, int> lc(3);
+  lrucache<char, short> lc(3);
 
   lc.insert_key_value_pair('b', 2);
   lc.insert_key_value_pair('a', 1);
@@ -61,4 +61,12 @@ TEST(lrucachetest, lrucache_should_work) {
   lc.insert_key_value_pair('a', 5);
 
   EXPECT_EQ(*lc.get_value_from_key('a'), 5);
+  
+  lc.insert_key_value_pair('x', 9);
+  lc.insert_key_value_pair('a', 6);
+  lc.insert_key_value_pair('z', 3);
+
+  EXPECT_EQ(lc.get_value_from_key('c'), nullptr);
+  EXPECT_EQ(*lc.get_most_recent_key(), 'z');
+  EXPECT_EQ(*lc.get_value_from_key('a'), 6);
 }
