f4793a5b5598d3eec34c49bf0f11e610403395ba
Maria <mlovatonv@gmail.com>
2020-09-11 16:02:24 -0500

    fix cache method

--- src/cache.cpp
+++ src/cache.cpp
@@ -4,15 +4,14 @@ namespace ads {
 
 template <typename t_key, typename t_value>
 void cache<t_key, t_value>::insert_key_value_pair(t_key key, t_value value) {
-  if (this->size == this->max_size) {
+  if (this->size == this->max_size and this->get_value_from_key(key) == nullptr) {
     this->evict();
-    --this->size;
-  }
-  if (this->get_value_from_key(key) == nullptr) {
-    ++this->size;
     this->insert(key, value);
-  } else {
+  } else if (this->get_value_from_key(key) != nullptr) {
     this->update(key, value);
+  } else {
+    this->insert(key, value);
+    ++this->size;
   }
 }
 
